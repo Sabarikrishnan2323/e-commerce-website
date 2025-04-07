d049dceb511f170fcc650b540bc2a5c35d0d7d45
Sabari <krish232003@gmail.com>
2025-04-07 15:56:58 +0530

    second commit

--- backend/build.sh
+++ backend/build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 set -o errexit
+cd backend
 
-pip install -r backend/requirements.txt
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
